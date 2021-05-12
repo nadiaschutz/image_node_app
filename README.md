970fe24ef2f81332a375263d3c0e8662de699a5b
happyDog <happydog@happyDog.ht.home>
2021-05-11 20:02:10 -0400

    adding to master

---
+++ README.md
@@ -0,0 +1,9 @@
+# IMAGE APP
+
+This app was created with Node.js, Express, MongoDB
+
+##HOW TO INSTALL
+run in terminal `npm i`
+
+##HOW TO RUN
+run in terminal `npm start`
---
+++ config.js
@@ -0,0 +1,4 @@
+const config={
+    MONGO_URI: "mongodb+srv://admin:admin@cluster0.qmnu6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
+module.exports=config;
